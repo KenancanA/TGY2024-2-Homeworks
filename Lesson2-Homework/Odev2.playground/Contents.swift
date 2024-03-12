0a9264b9e52f2ac11f54614d4376f6924cd9db7c
Kenancan Aslan <125500622+KenancanA@users.noreply.github.com>
2024-03-12 23:07:53 +0300

    test

---
+++ Lesson2-Homework/Odev2.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+/*2-Elimizde uzun bir cümle olsun. Bazı kelimelerin tekrar edildiğini düşünün. İstediğimiz ise hangi kelimeden kaç tane kullanıldığını bulmak.
+String = "Merhaba nasılsınız iyiyim siz nasılsınız bende iyiyim"
+Merhaba 1 kere, nasılsınız 2 kere iyiyim 2 kere*/
+
+let cumle : String = "Merhaba nasılsınız iyiyim siz nasılsınız bende iyiyim"
+
+let tKelime
+
+for bulma in x{
+    if
+}
+
+// index +1 if = remove
+
